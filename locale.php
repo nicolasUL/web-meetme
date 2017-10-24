9045ec8bdc11684d341b3719c213e50ddf5a186f
nicolasUL <nicolas.teissier@univ-lorraine.fr>
2017-10-24 14:37:42 +0200

    Use locale_accept_from_http function (php-intl) to identify locale

--- locale.php
+++ locale.php
@@ -1,16 +1,20 @@
 <?php
 //Default to English
 $locale = 'en_US';
-$languages = ($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-$languages = str_replace( ' ', '', $languages );
-$languages = str_replace( '-', '_', $languages );
-$languages = explode( ",", $languages );
+
+//$languages = ($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+//$languages = str_replace( ' ', '', $languages );
+//$languages = str_replace( '-', '_', $languages );
+//$languages = explode( ",", $languages );
+
+$languages = locale_accept_from_http($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
 
 //print "Locales = ";
 //print_r($languages);
 //print " <br>";
 
-foreach ( $languages as $temp) {
+/*foreach ( $languages as $temp) {
 	$temp = substr($temp,0,5);
 
 	$trans = './locale/'.$temp.'/LC_MESSAGES/messages.mo';
@@ -18,10 +22,19 @@ foreach ( $languages as $temp) {
 	//print "Looking for locale in $trans <br>";
 	if ( ($temp == "en_us") || file_exists($trans)) {
 		$locale = $temp;
-		break;
+		//break;
 	}
 
+}*/
+
+
+$trans = './locale/'.$languages.'/LC_MESSAGES/messages.mo';
+
+//print "Looking for locale in $trans <br>";
+if ( ($languages == "en_us") || file_exists($trans)) {
+	$locale = $languages;
 }
+
 
 setlocale(LC_ALL, $locale);
 
